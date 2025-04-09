9fe7e12c85aaf7d3862e3e086f6cc7b660eca3ba
Mazaljoker <122724127+Mazaljoker@users.noreply.github.com>
2025-04-09 12:37:33 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🧮 Open3CL Engine</h1>
+      <p>Le moteur est bien déployé sur Vercel 🎉</p>
+    </div>
+  );
+}
